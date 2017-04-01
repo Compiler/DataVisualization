2e90ae1fc2295554caa75d1546ae629ca12fbdde
compiler <lukerochedev@gmail.com>
2017-04-01 16:23:37 -0400

    water

---
+++ DataProject/Assets/Water.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+
+	private float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (0, 0, 0);
+	}
+	float dicks = 0.0f;
+	// Update is called once per frame
+	void Update () {
+		dicks += Time.deltaTime * speed;
+		transform.Translate (new Vector3 (dicks, dicks, dicks));
+	}
+
+}
